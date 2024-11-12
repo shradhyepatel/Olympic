d4f3b111a14165fe1724a24925dbfc88ff2f176a
Shradhye Patel <shradhyep@gmail.com>
2024-11-12 20:04:02 +0530

    Added Basic Side Bar Options

--- backend.py
+++ backend.py
@@ -4,15 +4,64 @@ import pandas as pd
 
 def medals(medal_tally):
 
-    #Dropping medals which are repeating for same team
     medal_tally = medal_tally.drop_duplicates(['Team' , 	'NOC', 	'Games',	'Year',	'Season',	'City',	'Sport'	,'Event' ,	'Medal'])
 
-    #Gropuping on region and calculating medals in Decending Order
     medal_tally = medal_tally.groupby('region').sum()[['Gold', 'Silver' , 'Bronze']].sort_values('Gold' , ascending = False ).reset_index()
 
-    #Calculating Total
     medal_tally['Total'] = medal_tally['Gold'] + medal_tally['Silver'] + medal_tally['Bronze']
 
     return medal_tally
+
+def country_list(df):
+
+    country = np.unique(df['region'].dropna()).tolist()
+    country.sort()
+    country.insert(0 , 'Overall')
+
+    year = np.unique(df['Year'].dropna()).tolist()
+    year.sort()
+    year.insert(0,'Overall')
+
+    return country , year
+
+def sport_analysis(df):
+    sport = df['Sport'].dropna().unique().tolist()
+    sport.sort()
+    sport.insert(0,'Overall')
+
+    country = np.unique(df['region'].dropna()).tolist()
+    country.sort()
+    country.insert(0 , 'Overall')
+
+    year = np.unique(df['Year'].dropna()).tolist()
+    year.sort()
+    year.insert(0,'Overall')
+
+    return sport , country , year
+
+def athlete_analysis(df):
+    name = df['Name'].dropna().unique().tolist()
+    name.sort()
+    name.insert(0,'Overall')
+
+    year = np.unique(df['Year'].dropna()).tolist()
+    year.sort()
+    year.insert(0,'Overall')
+
+    return name , year
+
+def gender_analysis(df):
+    sport = np.unique(df['Sport'].dropna()).tolist()
+    sport.sort()
+    sport.insert(0,'Overall')
+
+    country = np.unique(df['region'].dropna()).tolist()
+    country.sort()
+    country.insert(0 , 'Overall')
+
+    return sport , country
+    
+
+
 
 
--- main.py
+++ main.py
@@ -7,16 +7,37 @@ import backend
 
 df = preprocess.initialize()
 
-st.title('Olympic Anaylasis')
-
 st.sidebar.title('Olympics Analysis')
-opt = st.sidebar.radio('Select An Option' , ['Medal Tally' , 'General Trends'])
+opt = st.sidebar.radio('Select An Option' , ['Medal Tally' , 'Country-Wise Analysis' , 'Sport-Wise Analysis' , 'Athlete-Wise Analysis' , 'Gender-Wise Analysis'])
 
-if opt:
-    if opt == 'Medal Tally':
-        responce = backend.medals(df)
-        st.dataframe(responce)
-    else :
-        st.text(opt)
+if opt == 'Medal Tally':
+     st.title('Medal Tally')
+     responce = backend.medals(df)
+
+     st.dataframe(responce)
+
+elif opt == 'Country-Wise Analysis':
+    country , year = backend.country_list(df)
+    st.title('Country Analysis')
+
+    st.sidebar.selectbox('Select a Country' , country)
+    st.sidebar.selectbox('Select a Year' , year)
+
+elif opt == 'Sport-Wise Analysis':
+    sport , country , year = backend.sport_analysis(df)
+    st.title('Sport Analysis')
+
+    st.sidebar.selectbox('Select a Sport' , sport)
+    st.sidebar.selectbox('Select a Country' , country)
+    st.sidebar.selectbox('Select a Year' , year)
+
+elif opt == 'Gender-Wise Analysis':
+    sport , country = backend.gender_analysis(df)
+    st.title('Athlete Analysis')
+
+    st.sidebar.selectbox('Select a Gender' , ['Both' , 'Male' , 'Female'])
+    st.sidebar.selectbox('Select a Sport' , sport)
+    st.sidebar.selectbox('Select a Country' , country)
+
 
 
