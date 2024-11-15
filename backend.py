2759c1331e1d9bc3f6558c76d6a02273ef728929
Shradhye Patel <shradhyep@gmail.com>
2024-11-15 20:02:51 +0530

    completed country overall

--- backend.py
+++ backend.py
@@ -1,13 +1,17 @@
 import numpy as np 
 import pandas as pd 
+import plotly.express as px
 
+def unique(df):
+     
+     df = df.drop_duplicates(['Team' , 	'NOC', 	'Games',	'Year',	'Season',	'City',	'Sport'	,'Event' ,	'Medal'])
 
-def medals(medal_tally):
+     return df
 
-    medal_tally = medal_tally.drop_duplicates(['Team' , 	'NOC', 	'Games',	'Year',	'Season',	'City',	'Sport'	,'Event' ,	'Medal'])
+def medals(df):
 
+    medal_tally = unique(df)
     medal_tally = medal_tally.groupby('region').sum()[['Gold', 'Silver' , 'Bronze']].sort_values('Gold' , ascending = False ).reset_index()
-
     medal_tally['Total'] = medal_tally['Gold'] + medal_tally['Silver'] + medal_tally['Bronze']
 
     return medal_tally
@@ -24,7 +28,7 @@ def country_list(df):
 
     return country , year
 
-def sport_analysis(df):
+def sport_list(df):
     sport = df['Sport'].dropna().unique().tolist()
     sport.sort()
     sport.insert(0,'Overall')
@@ -39,7 +43,7 @@ def sport_analysis(df):
 
     return sport , country , year
 
-def athlete_analysis(df):
+def athlete_list(df):
     name = df['Name'].dropna().unique().tolist()
     name.sort()
     name.insert(0,'Overall')
@@ -50,7 +54,7 @@ def athlete_analysis(df):
 
     return name , year
 
-def gender_analysis(df):
+def gender_list(df):
     sport = np.unique(df['Sport'].dropna()).tolist()
     sport.sort()
     sport.insert(0,'Overall')
@@ -60,6 +64,55 @@ def gender_analysis(df):
     country.insert(0 , 'Overall')
 
     return sport , country
+
+def country_analysis(country , df):
+        temp_df = df[df['region'] == country]
+        graph = temp_df['Year'].value_counts()
+        no_of_players = px.bar(graph)
+        no_of_players.update_layout(
+                            xaxis_title="Year",
+                            yaxis_title="No of Players")
+        
+        return no_of_players
+
+def overall_analysis(df):
+        
+        #df = unique(df)
+
+        graph = df['Year'].value_counts()
+        no_of_players = px.bar(graph)
+        no_of_players.update_layout(
+                            xaxis_title="Year",
+                            yaxis_title="No of Players")
+        
+        fig = px.histogram(df  , x = 'Year' ,  color = 'Sex')
+        fig.update_layout(
+                  xaxis_title="Year",
+                  yaxis_title="No of Players")
+        
+        meda_tally = pd.concat([df, pd.get_dummies(df['Medal'])], axis=1)
+        meda_tally = meda_tally.drop_duplicates(['Team', 'NOC', 'Games', 'Year', 'Season', 'City', 'Sport', 'Event', 'Medal'])
+        meda_tally = meda_tally.groupby('region').sum()[['Gold', 'Silver', 'Bronze']]
+        meda_tally['Total'] = meda_tally[['Gold', 'Silver', 'Bronze']].sum(axis=1)/2
+        meda_tally = meda_tally.drop(['Gold', 'Silver', 'Bronze'], axis=1).sort_values(by='Total', ascending=False).reset_index()
+        top_10 = meda_tally.head(10)
+        top_10_chart = px.bar(top_10, x='region', y='Total', color='region')
+
+        top_10_chart.update_layout(
+            xaxis_title="Region", 
+            yaxis_title="Total Medals" 
+        )
+
+        medals_graph= px.histogram(df , x = 'Medal' , color = 'Sex')
+        medals_graph.update_layout(
+            xaxis_title="Medals", 
+            yaxis_title="Total Medals" 
+        )
+        
+        return no_of_players , fig , top_10_chart , medals_graph
+    
+
+
     
 
 
--- main.py
+++ main.py
@@ -14,26 +14,57 @@ if opt == 'Medal Tally':
      st.title('Medal Tally')
      responce = backend.medals(df)
 
-     st.dataframe(responce)
+     st.dataframe(responce , use_container_width=True , hide_index= True)
 
 elif opt == 'Country-Wise Analysis':
     country , year = backend.country_list(df)
-    st.title('Country Analysis')
+    st.title('Country-Wise Analysis')
 
-    st.sidebar.selectbox('Select a Country' , country)
-    st.sidebar.selectbox('Select a Year' , year)
+    country = st.sidebar.selectbox('Select a Country' , country)
+    year = st.sidebar.selectbox('Select a Year' , year)
+    st.subheader(country)
+
+    if country == 'Overall':
+        no_of_players , fig , top_10_chart , medals_graph = backend.overall_analysis(df)
+
+        st.text('Total no of players participated each year')
+        st.plotly_chart(no_of_players)
+
+        st.text('Total no of Males and Females participated every year')
+        st.plotly_chart(fig)
+
+        st.text('Top 10 countries with highest no of Total medals')
+        st.plotly_chart(top_10_chart)
+        
+        st.text('Medal distribution between Male and Female')
+        st.plotly_chart(medals_graph)
+        
+
+
+    else:
+        responce = backend.country_analysis(country , df)
+        st.plotly_chart(responce)
+
+    
 
 elif opt == 'Sport-Wise Analysis':
-    sport , country , year = backend.sport_analysis(df)
-    st.title('Sport Analysis')
+    sport , country , year = backend.sport_list(df)
+    st.title('Sport-Wise Analysis')
 
     st.sidebar.selectbox('Select a Sport' , sport)
     st.sidebar.selectbox('Select a Country' , country)
     st.sidebar.selectbox('Select a Year' , year)
 
+elif opt == 'Athlete-Wise Analysis':
+    name, year = backend.athlete_list(df)
+    st.title('Athlete-Wise Analysis')
+
+    st.sidebar.selectbox('Select a Sport' , name)
+    st.sidebar.selectbox('Select a Year' , year)
+
 elif opt == 'Gender-Wise Analysis':
-    sport , country = backend.gender_analysis(df)
-    st.title('Athlete Analysis')
+    sport , country = backend.gender_list(df)
+    st.title('Gender-Wise Analysis')
 
     st.sidebar.selectbox('Select a Gender' , ['Both' , 'Male' , 'Female'])
     st.sidebar.selectbox('Select a Sport' , sport)
